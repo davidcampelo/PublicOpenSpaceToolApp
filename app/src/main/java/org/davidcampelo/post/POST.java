5f9ddbdbe38658a5738de779b9a134e4ff587561
David Campelo <david.campelo@gmail.com>
2016-08-30 00:08:32 +0100

    Removed old image from layout

---
+++ app/src/main/java/org/davidcampelo/post/POST.java
@@ -0,0 +1,7 @@
+package org.davidcampelo.post;
+
+/**
+ * Created by davidcampelo on 8/29/16.
+ */
+public class POST {
+}
---
+++ app/src/main/java/org/davidcampelo/post/utils/FontsOverride.java
@@ -0,0 +1,7 @@
+package org.davidcampelo.post.utils;
+
+/**
+ * Created by davidcampelo on 8/29/16.
+ */
+public class FontsOverride {
+}
