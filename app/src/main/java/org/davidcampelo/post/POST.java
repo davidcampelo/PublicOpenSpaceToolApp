f4655d3e3950c84c6a711a67167d0741e2578a39
David Campelo <david.campelo@gmail.com>
2016-08-31 23:31:39 +0100

    New layut theme

--- app/src/main/java/org/davidcampelo/post/POST.java
+++ app/src/main/java/org/davidcampelo/post/POST.java
@@ -1,7 +1,20 @@
 package org.davidcampelo.post;
 
+import android.app.Application;
+
+import org.davidcampelo.post.utils.FontsOverride;
+
 /**
  * Created by davidcampelo on 8/29/16.
  */
-public class POST {
+public class POST extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/MyFontAssets.ttf");
+//        FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/MyFontAssets.ttf");
+//        FontsOverride.setDefaultFont(this, "SERIF", "fonts/MyFontAssets.ttf");
+//        FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/MyFontAssets.ttf");
+
+    }
 }
