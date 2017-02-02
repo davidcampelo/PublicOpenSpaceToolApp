3583707269fa7344212f11a32a67f111edf729d4
David Campelo <david.campelo@gmail.com>
2017-02-02 22:36:13 +0000

    Export data screen: sending codebook with the CSV generated file

--- app/src/main/java/org/davidcampelo/post/utils/ProjectExportAsyncTask.java
+++ app/src/main/java/org/davidcampelo/post/utils/ProjectExportAsyncTask.java
@@ -3,14 +3,23 @@ package org.davidcampelo.post.utils;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.davidcampelo.post.R;
 import org.davidcampelo.post.model.Project;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 /**
@@ -20,6 +29,7 @@ import java.util.ArrayList;
 public class ProjectExportAsyncTask extends AsyncTask<String, String, String> {
 
     ProgressDialog progressDialog;
+    File externalStorage = Environment.getExternalStorageDirectory();
 
     Context context;
     Project project;
@@ -34,6 +44,55 @@ public class ProjectExportAsyncTask extends AsyncTask<String, String, String> {
         this.csv = csv;
         this.kmz = kmz;
         this.sql = sql;
+    }
+
+
+    /**
+     *  Copy a File from assets directory to the context.getExternalCacheDir() folder
+      */
+    private void copyAssets(String myFileName) throws Exception {
+        File file = null;
+        AssetManager assetManager = context.getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            throw new Exception("ERROR: Codebook could NOT be opened from assets directory!");
+        }
+        for(String filename : files) {
+            InputStream in = null;
+            if (filename.equals(myFileName)) {
+                file = new File(context.getExternalCacheDir() + "/"+ myFileName);
+                if (!file.exists()) {
+                    FileOutputStream out = null;
+                    try {
+                        out = (FileOutputStream) new FileOutputStream(file);
+                    } catch (FileNotFoundException e) {
+                        throw new Exception("ERROR: Codebook could NOT be created!");
+                    }
+                    try {
+                        in = assetManager.open(filename);
+                        copyFile(in, out);
+                    } catch (Exception e) {
+                        throw new Exception("ERROR: Codebook could NOT be copied!");
+                    }
+                    finally {
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            }
+        }
+    }
+    private void copyFile(InputStream in, FileOutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
     }
 
     @Override
@@ -56,6 +115,8 @@ public class ProjectExportAsyncTask extends AsyncTask<String, String, String> {
                 publishProgress("Creating CSV file...");
                 File csvFile = CSVUtils.export( context, project);
                 listFilesToExport.add(Uri.fromFile(csvFile));
+                copyAssets("POST_codebook.pdf");
+                listFilesToExport.add(Uri.fromFile(new File(context.getExternalCacheDir() + "/POST_codebook.pdf")));
             }
              if (sql) {
                 publishProgress("Creating SQL file...");
@@ -79,7 +140,7 @@ public class ProjectExportAsyncTask extends AsyncTask<String, String, String> {
         } catch (Exception e) {
             Log.e("ERROR", "", e);
             progressDialog.dismiss();
-            return "ERROR!";
+            return "ERROR: "+ e.getMessage();
         }
     }
 
